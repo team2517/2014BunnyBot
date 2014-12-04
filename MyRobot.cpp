4bff1db47807dced2a016123aa4bb02d2a447b3b
tylerscheuble <tylerscheuble@gmail.com>
2014-12-03 19:46:47 -0800

    Simple robot template + jaguars

---
+++ MyRobot.cpp
@@ -0,0 +1,64 @@
+#include "WPILib.h"
+#define FLJaguarID				1
+#define FRJaguarID				2
+#define BLJaguarID				3
+#define BRJaguarID				4
+
+
+/**
+ * This is a demo program showing the use of the RobotBase class.
+ * The SimpleRobot class is the base of a robot application that will automatically call your
+ * Autonomous and OperatorControl methods at the right time as controlled by the switches on
+ * the driver station or the field controls.
+ */ 
+class RobotDemo : public SimpleRobot  // Jaguars and such go here
+{
+	Joystick stick; // only joystick
+	
+	CANJaguar frontLeft;
+	CANJaguar frontRight;
+	CANJaguar backLeft;
+	CANJaguar backRight;
+
+	
+public:
+	RobotDemo():
+		stick(1), frontLeft(FLJaguarID), frontRight(FRJaguarID), 
+	{
+		myRobot.SetExpiration(0.1);
+	}
+
+	/**
+	 * Drive left & right motors for 2 seconds then stop
+	 */
+	void Autonomous()
+	{
+		myRobot.SetSafetyEnabled(false);
+		myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
+		Wait(2.0); 				//    for 2 seconds
+		myRobot.Drive(0.0, 0.0); 	// stop robot
+	}
+
+	/**
+	 * Runs the motors with arcade steering. 
+	 */
+	void OperatorControl()
+	{
+		myRobot.SetSafetyEnabled(true);
+		while (IsOperatorControl())
+		{
+			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+			Wait(0.005);				// wait for a motor update time
+		}
+	}
+	
+	/**
+	 * Runs during test mode
+	 */
+	void Test() {
+
+	}
+};
+
+START_ROBOT_CLASS(RobotDemo);
+
